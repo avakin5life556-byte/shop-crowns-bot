80023db863b247ea55881848a6c7b654a39db66f
avakin5life556-byte <avakin5life556@gmail.com>
2026-04-28 15:53:44 +0300

    Update states.py

--- states.py
+++ states.py
@@ -1,13 +1,31 @@
 from aiogram.dispatcher.filters.state import State, StatesGroup
 
-
-class AdminReplyStates(StatesGroup):
-    WAITING_BROADCAST = State()
-
-
+# ========== Free Orders States ==========
 class ChangeNameStates(StatesGroup):
-    waiting_for_name = State()
+    CHECK_BALANCE = State()
+    WAITING_NAME = State()
+    WAITING_EMAIL = State()
+    WAITING_PASSWORD = State()
 
 
 class ChangePhotoStates(StatesGroup):
-    waiting_for_photo = State()
+    CHECK_BALANCE = State()
+    WAITING_PHOTO = State()
+    WAITING_EMAIL = State()
+    WAITING_PASSWORD = State()
+
+
+# ========== Support States ==========
+class ComplaintStates(StatesGroup):
+    WAITING_MESSAGE = State()
+
+
+class LiveChatStates(StatesGroup):
+    ACTIVE = State()
+
+
+# ========== Admin Reply States ==========
+class AdminReplyStates(StatesGroup):
+    WAITING_REPLY = State()
+    WAITING_BAN_REASON = State()
+    WAITING_BROADCAST = State()
